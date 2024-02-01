a2f02fc0f4bbdb7ef35f90ca6fa05965a2604dbc
J0n1lu5 <j.pluder@mci4me.at>
2024-02-01 11:39:08 +0100

    database implemented

---
+++ Songs.py
@@ -0,0 +1,59 @@
+import os
+from tinydb import TinyDB, Query
+from serializer import serializer
+
+
+class Song ():
+    db_connector = TinyDB(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.json'), storage=serializer).table('Songs')
+
+    def __init__ (self, song_name : str, interpret : str):
+        self.song_name = song_name
+        # The user id of the user that manages the device
+        # We don't store the user object itself, but only the id (as a key)
+        self.interpret = interpret
+        self.is_active = True
+
+    def __str__(self):
+            return f'Device {self.song_name} ({self.interpret})'
+
+    def __repr__(self):
+            return self.__str__()
+
+    def store_data(self):
+            print("Storing data...")
+            # Check if the song already exists in the database
+            SongQuery = Query()
+            result = self.db_connector.search(SongQuery.song_name == self.song_name)
+            if result:
+                # Update the existing record with the current instance's data
+                result = self.db_connector.update(self.__dict__, doc_ids=[result[0].doc_id])
+                print("Data updated.")
+            else:
+
+                
+                # If the song doesn't exist, insert a new record
+                self.db_connector.insert(self.__dict__)
+                print("Data inserted.")
+
+
+
+    def delete_data(self):
+        print("Deleting data...")
+        SongQuery = Query()
+        result = self.db_connector.remove(SongQuery.song_name == self.song_name)
+        print(f"Deleted {result}")
+
+
+
+    @classmethod
+    def load_data_by_device_name(cls, song_name):
+        # Load data from the database and create an instance of the Song class
+        SongQuery = Query()
+        result = cls.db_connector.search(SongQuery.song_name == song_name)
+
+        if result:
+            data = result[0]
+            return cls(data['song_name'], data['interpret'])
+        
+        else:
+            return None
--- user_interface.py
+++ user_interface.py
@@ -1,8 +1,10 @@
-### Erste Streamlit App
 
 import streamlit as st
-import pandas as pd
-from io import StringIO
+from duckduckgo_search import DDGS
+import io
+import requests
+from PIL import Image
+from Songs import Song
 
 
 # Eine Überschrift der ersten Ebene
@@ -13,14 +15,60 @@ tab1, tab2 = st.tabs(["hochladen", "erkennen"])
 
 
 with tab1:
-     st.write("hochladen")
+    st.write("hochladen")
+    
+    with st.form ("upload"):
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+        uploaded_file = st.file_uploader("Choose a file")
+        if uploaded_file is not None:
+            #fingerprint erstellen und ablegen in der datenbank
+            st.write("file ausgewählt")
+        
+        
+        title = st.text_input("# Title") 
+        
+        interpret = st.text_input("# Interpret")   
 
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+
+            new_song= Song(title,interpret)
+            new_song.store_data()
+            st.write("upload complete")    
+            st.rerun()
+
+
+        if title and interpret is not None:
+            with DDGS() as ddgs:
+                keywords = f"{title} {interpret} album cover"
+                ddgs_images_gen = ddgs.images(
+                    keywords,
+                    region="wt-wt",
+                    safesearch="off",
+                    size=None,
+                    color="Monochrome",
+                    type_image=None,
+                    layout=None,
+                    license_image=None,
+                    max_results=1,
+                )
+                images = list(ddgs_images_gen)
+                if images:
+                    image_url = images[0].get('image')
+                    if image_url:
+                        # Fetch the image data
+                        response = requests.get(image_url)
+                        image_bytes_io = io.BytesIO(response.content)
+                        
+                        # Open the image using PIL
+                        pil_image = Image.open(image_bytes_io)
+                        
+                        # Display the image in Streamlit
+                        st.image(pil_image, caption='Album Cover', use_column_width=True)
+                    else:
+                        st.write("Album Cover URL nicht gefunden.")
+                else:
+                    st.write("Album Cover nicht gefunden.")
    
 
  
